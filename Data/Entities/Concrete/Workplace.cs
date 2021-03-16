01583ec2ce57b821006387ca91053fcff0f588e0
AliOzbek <alimustafaozbek@gmail.com>
2021-03-16 18:04:36 +0300

    Layers

---
+++ Data/Entities/Concrete/Workplace.cs
@@ -0,0 +1,17 @@
+﻿using Data.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Entities.Concrete
+{
+    public class Workplace : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AuthorizedPerson { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public int FaxNumber { get; set; }
+    }
+}
